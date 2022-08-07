4433936593dda42d549cadb4f3e14542fb04707a
Warspiteful <jalmendral24@gmail.com>
2022-08-06 20:41:27 -0400

    Frontview

--- LRJ2022Project/Assets/Scripts/TopDown/RailgunControl.cs
+++ LRJ2022Project/Assets/Scripts/TopDown/RailgunControl.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class RailgunControl : MonoBehaviour
 {
-   public void TryFire(int lane)
+    public List<Vector3> lanePositions;
+    [SerializeField] private IntVariable railgunLane;
+   public void TryFire()
    {
-      
-   }
+
+       Debug.Log("Fired");
+       RaycastHit[] hitInfo = Physics.BoxCastAll(lanePositions[railgunLane.Value],
+           new Vector3(8, 32, 32), Vector3.forward);
+       
+                    if(hitInfo.Length > 0 ){
+                        foreach(RaycastHit hit in hitInfo)
+                        {
+                            if (hit.collider != null)
+                            {
+                                Debug.Log(hit.collider.gameObject.name);
+                            }
+                        }
+                    }
+                }
+   
 }
