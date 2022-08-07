42d99b03bef804ff877a5dad11da10ba1f79a6d8
Kane Xu <kanexu.work@gmail.com>
2022-08-07 11:59:07 -0400

    2 Wrong Implementation But Keeping the Code for Reference

---
+++ LRJ2022Project/Assets/Scripts/Weapons/WeaponData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WeaponData")]
+public class WeaponData : ScriptableObject
+{
+    public int remainingBullets;
+    public string weaponName;
+    public int bulletCapacity;
+    public float weaponCooldown;
+    public float weaponReloadTime;
+    
+    public void Fire()
+    {
+        remainingBullets -= 1;
+    }
+
+    public bool NeedReload()
+    {
+        return remainingBullets <= 0;
+    }
+}
