b6357ef6338064493c24cef6e32d3683f3eea60e
Warspiteful <jalmendral24@gmail.com>
2022-08-06 22:18:39 -0400

    Integrated Damage System and FSM Perspective Switch

--- LRJ2022Project/Assets/Scripts/StateMachine/States/PlayerBaseState.cs
+++ LRJ2022Project/Assets/Scripts/StateMachine/States/PlayerBaseState.cs
@@ -1,3 +1,4 @@
+
 public abstract class PlayerBaseState
 {
     public PerspectiveEnum Perspective;
--- LRJ2022Project/Assets/Scripts/TopDown/RailgunControl.cs
+++ LRJ2022Project/Assets/Scripts/TopDown/RailgunControl.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class RailgunControl : MonoBehaviour
 {
-    public List<Vector3> lanePositions;
+    [SerializeField] private List<Vector3> lanePositions;
     [SerializeField] private IntVariable railgunLane;
+
+    [SerializeField] private int damage; 
    public void TryFire()
    {
 
        Debug.Log("Fired");
        RaycastHit[] hitInfo = Physics.BoxCastAll(lanePositions[railgunLane.Value],
            new Vector3(8, 32, 32), Vector3.forward);
-       
-                    if(hitInfo.Length > 0 ){
-                        foreach(RaycastHit hit in hitInfo)
-                        {
-                            if (hit.collider != null)
-                            {
-                                Debug.Log(hit.collider.gameObject.name);
-                            }
-                        }
-                    }
+
+        if(hitInfo.Length > 0 ){
+            foreach(RaycastHit hit in hitInfo)
+            {
+                if (hit.collider != null && hit.collider.gameObject.GetComponent<Enemy>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<Enemy>().TakeDamage(damage);
                 }
-   
+                {
+                    
+                }
+            }
+        }
+    }
 }
